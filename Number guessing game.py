48e5fc8a8f0e5b321cf7734749fedd6f63fc18b9
𝓖𝓸𝓴𝓾𝓵𝓪𝓴𝓪𝓷𝓷𝓪𝓷 <gokulakannan2008cs@gmail.com>
2024-10-29 21:26:33 +0530

    Update Number guessing game.py

--- Number guessing game.py
+++ Number guessing game.py
@@ -1,4 +1,5 @@
-from tkinter import *
+# version 1
+"""from tkinter import *
 import random
 
 root = Tk()
@@ -35,4 +36,53 @@ entrybox.grid(row=0, column=1, padx=10, pady=15)
 
 submit = Button(root, text="Play", command=computer, font=("Ariel", 25, "bold"), background="brown", width=23)
 submit.grid(row=1, column=0, columnspan=4, padx=10)
+root.mainloop()
+"""
+# version 2
+from customtkinter import *
+import random
+from tkinter import messagebox
+
+root = CTk()
+root.geometry("650x300")
+root.resizable(False, False)
+root.configure(bg="sky blue")
+root.title("Number guessing game")
+
+
+main_frame = CTkFrame(root, height=250, width=600, fg_color="ivory4")
+main_frame.place(x=25, y=25)
+
+
+
+def computer():
+    gamer1 = entrybox.get()
+    if gamer1 == "":
+        messagebox.showerror("Input error", "Enter a number")
+
+    gamer = int(entrybox.get())
+    com = random.randint(0, 9)
+    a = ("You", gamer, "Computer", com)
+    label2 = CTkLabel(main_frame, text="", font=("Ariel", 18, "bold"), fg_color="ivory4")
+    label2.place(x=200, y=150)
+
+    label3 = CTkLabel(main_frame, text="", font=("Ariel", 18, "bold"), fg_color="ivory4")
+    label3.place(x=260, y=190)
+    if 0 <= gamer <= 9:
+        if com == gamer:
+            label3.configure(text="Win")
+            label2.configure(text=a)
+        else:
+            label2.configure(text=a)
+            label3.configure(text="Out")
+
+
+label = CTkLabel(main_frame, text="Enter a you guessed number ", font=("Ariel", 18, "bold"))
+label.place(x=25, y=25)
+
+entrybox = CTkEntry(main_frame, font=("Ariel", 18, "bold"), corner_radius=2)
+entrybox.place(x=290, y=25)
+
+submit = CTkButton(root, text="Play", command=computer, font=("Ariel", 25, "bold"), bg_color="ivory4", width=90)
+submit.place(x=260, y=110)
 root.mainloop()
